31b56d049300608f410d9906fc49fc95b5fc978b
sebas2426 <tobiaspalma505@gmail.com>
2024-09-06 17:21:01 -0400

    Update and optimize the code

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -17,13 +17,19 @@ rgbButton.classList="rgbButton";
 rgbButton.textContent="Rainbow Mode";
 containerFooter.appendChild(rgbButton);
 
+const eraserButton= document.createElement("button");
+eraserButton.classList="eraserButton";
+eraserButton.textContent="Eraser";
+containerFooter.appendChild(eraserButton);
+
 const clearButton= document.createElement("button");
 clearButton.classList="clearButton";
 clearButton.textContent="Clear";
 containerFooter.appendChild(clearButton);
 
-divsSize(16);
 let userInput;
+
+divsSize(16);
 
 askingButton.addEventListener("click", () =>{
      userInput= prompt("Put the size of the sketch board ");
@@ -37,46 +43,62 @@ askingButton.addEventListener("click", () =>{
 });
 
 function divsSize(Size){
+    const fragment= document.createDocumentFragment();
         for(let i=0;i<Size * Size;i++){
             let divs= document.createElement("div");
             divs.classList="sketchDivs";
             divs.style.setProperty('height',`calc(100%/${Size})`);
             divs.style.setProperty('width',`calc(100%/${Size})`);
-            container.appendChild(divs);
+            fragment.appendChild(divs);
+        };
 
-            //Normal color
-            divs.addEventListener('mouseover', () => {
-            divs.style.setProperty('background-Color', 'black');
-            });
-
-            //Normal Color button
-            normalColorButton.addEventListener("click", () =>{
-                divs.addEventListener('mouseover', () => {
-                    divs.style.setProperty('background-Color', 'black');
-                    });
-            })
-
-            //RGB option
-            rgbButton.addEventListener("click", () =>{
-                rgbButton.style.setProperty('background-Color', 'white');
-                divs.addEventListener('mouseover', () => {
-                    divs.style.setProperty('background-Color',`rgb(${randomRGBColors()},${randomRGBColors()},${randomRGBColors()})`);
-                     });
-            });
-
-            //Clear the divs
-            clearButton.addEventListener("click", () =>{
-                divs.style.setProperty('background-Color', 'white');
-            });
+          //Normal color
+    container.addEventListener('mouseover', (event) => {
+        if(event.target.classList.contains("sketchDivs")){
+            event.target.style.backgroundColor="black";
         }
+    });
+
+    //Normal Color button
+    normalColorButton.addEventListener("click", () =>{
+        container.addEventListener('mouseover', (event) => {
+            if(event.target.classList.contains("sketchDivs")){
+                event.target.style.backgroundColor="black";
+            }
+            });
+    })
+
+    //RGB option
+    rgbButton.addEventListener("click", () =>{
+        container.addEventListener('mouseover', (event) => {
+            if(event.target.classList.contains("sketchDivs")){
+                event.target.style.backgroundColor=`rgb(${randomRGBColors()},${randomRGBColors()},${randomRGBColors()})`;
+            }
+             });
+    });
+
+    //Eraser
+    eraserButton.addEventListener("click", () =>{
+        container.addEventListener("mouseover", (event) =>{
+            if(event.target.classList.contains("sketchDivs")){
+                event.target.style.backgroundColor="white";
+            }
+        });
+    });
+
+    //Clear the divs
+    clearButton.addEventListener("click", () =>{
+        const divs = document.querySelectorAll('.sketchDivs');
+        divs.forEach((div) => {
+            div.style.backgroundColor = 'white'; 
+        });
+    });
+
+container.appendChild(fragment);
 }
 
-
-
 function deleteDivs(){
-    for(let i=0;i<userInput* userInput;i++){
-        container.textContent='';
-    }
+    container.replaceChildren();
 };
 function randomRGBColors(){
     let randomColor= Math.round(Math.random()*255);
